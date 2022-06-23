06c8863bbaa0be9efcb9a16ac9ed1acc39d987d1
GabrielScariot <106630792+GabrielScariot@users.noreply.github.com>
2022-06-23 16:39:19 -0300

    Add files via upload

---
+++ Dia1TERMOO.cs
@@ -0,0 +1,96 @@
+﻿// See https://aka.ms/new-console-template for more information
+main();
+
+int lerNumeroInteiro(){
+    int numero =int.Parse(Console.ReadLine());
+    return numero;
+}
+
+int [] lerSenhaDigitada(){
+    int [] senhaDigitada = new int[5];
+    for(int i=0;i<5;i++){
+        Console.WriteLine("Dgiite o valor da posicao " + (i+1)+":");
+        senhaDigitada[i]=lerNumeroInteiro();
+    }   
+
+    return senhaDigitada;
+}
+
+void Contador(){
+    for(int i=0;i<5;i++){
+        Console.WriteLine(i);
+    }
+} 
+
+int[] gerarSenha(){
+    Random randNumero = new Random();
+    int[] senha = new int[5];
+    for(int i=0;i<5;i++){
+        senha[i] =randNumero.Next(10); 
+        for(int j=i-1;j>=0;j--){
+            if(senha[i]==senha[j]){
+                i--;
+                j=0;
+            }
+        }
+                
+    }
+   return senha;
+}
+
+char [] comparaSenha(int [] digitado, int [] senha){
+    char [] resposta = new char[5];
+    for(int i=0;i<5;i++){
+       resposta[i]='N'; 
+        for(int j=0;j<5;j++){
+            if(digitado[i]==senha[j]){
+                if(i==j){
+                    resposta[i]='c';
+                }
+                else{
+                    resposta[i]='p';
+
+                }
+                
+            }
+        }
+                
+    }
+    return resposta;
+}
+
+bool verificaVitoria(char [] resposta){
+    for(int i =0;i<5;i++){
+        if(resposta[i]!='c'){
+            return false;
+        }
+    }
+    return true;
+}
+void exibirSenha(int []senha){
+      for(int i=0;i<5;i++){
+        Console.WriteLine(senha[i]);
+    }
+
+}
+
+int main(){
+    //int numero = lerNumeroInteiro();
+    bool vitoria = false;
+    int[] senha = gerarSenha();
+    int[] senhaDigitada = new int[5];
+    char[] resposta = new char[5];
+     while(!vitoria){
+        senhaDigitada = lerSenhaDigitada();
+        resposta= comparaSenha(senhaDigitada,senha); 
+        vitoria = verificaVitoria(resposta);
+    
+        Console.Clear();
+        exibirSenha(senhaDigitada);
+        Console.WriteLine("");
+        Console.WriteLine(resposta);   
+    }
+    Console.WriteLine("Voce venceu");
+    Console.ReadKey();
+    return 0;
+}
---
+++ Dia2CADASTRO.cs
@@ -0,0 +1,151 @@
+﻿
+main();
+
+int main(){
+    
+    /*Endereco endereco1= new Endereco();
+    endereco1.exibirEndereco();
+
+    Endereco endereco2= new Endereco(54, "Parigot", "Porto Alegre", "Toledo");
+    endereco2.exibirEndereco();*/
+    /*Endereco end = new Endereco(54, "Parigot", "Porto Alegre", "Toledo");
+
+    Pessoa pessoa1= new Pessoa("Joao", "655666", end);
+
+    pessoa1.getDados();
+
+    pessoa1.setSenha("1254");
+
+    pessoa1.getDados();*/
+    
+
+    Pessoa test = new Pessoa();
+
+    test.setNome();
+    test.setSenha();
+    test.setRua();
+    test.setNumero();
+    test.setBairro();
+    test.setCidade();
+    
+    test.getDados();
+
+    return 0;
+}
+
+
+class Endereco{
+    private int numero;
+    private string rua;
+    private string bairro;
+    private string cidade;
+    public Endereco(){
+        this.numero=0;
+        this.rua ="";
+        this.bairro= "";
+        this.cidade ="";
+    }
+    public Endereco(int numero, string rua, string bairro, string cidade){
+        this.numero= numero;   
+        this.rua = rua;
+        this.bairro= bairro;
+        this.cidade = cidade;
+    }
+    public string getCidade(){return this.cidade;}
+    public int getNumero(){return this.numero;}
+    public string getBairro(){return this.bairro;}
+    public string getRua(){return this.rua;}
+    public void setNumero(int numero){this.numero=numero;}
+    public void setRua(string rua){this.rua=rua;}
+    public void setCidade(string cidade){this.cidade=cidade;}
+    public void setBairro(string bairro){this.bairro=bairro;}
+    public void exibirEndereco(){
+        Console.WriteLine("Numero: "+ getNumero()+ " Rua: "+ getRua()+ " Bairro: " + getBairro()+ " Cidade: "+ getCidade());
+    }
+}
+
+class Pessoa : Endereco{
+    private string nome;
+
+    private string senha;
+
+    private Endereco endereco;
+
+    public Pessoa(){
+        this.nome="";
+        this.senha="";
+        this.endereco= new Endereco();
+    }
+
+    public Pessoa(string nome, string senha,Endereco endereco){
+        this.senha=senha;
+
+        this.nome=nome;
+        this.endereco= endereco;
+    }
+
+    public string getNome(){return this.nome;}
+
+    public string getSenha(){return this.senha;}
+
+    public void getEndereco(){
+        endereco.exibirEndereco();
+
+    }
+
+    public void getDados(){
+
+        Console.WriteLine("Nome: "+getNome()+ " Senha:"+ senha);
+
+       this.endereco.exibirEndereco();
+
+    }
+
+    public void setSenha()
+    {
+        Console.WriteLine("Digite sua senha:");
+        senha = Console.ReadLine();
+    }
+    public void setNome(){
+        Console.WriteLine("Digite seu nome:");
+        this.nome= Console.ReadLine();
+
+
+    }
+    public void setNumero(){
+        Console.WriteLine("Digite o numero da sua casa");
+
+        int numero = int.Parse(Console.ReadLine());
+
+        this.endereco.setNumero(numero);
+        
+        }
+    public void setRua(){
+        Console.WriteLine("Digite sua rua");
+        string rua= Console.ReadLine();
+
+        this.endereco.setRua(rua);
+
+
+
+    }
+    public void setCidade(){
+        Console.WriteLine("Digite sua cidade");
+        string cidade= Console.ReadLine();
+    
+        this.endereco.setCidade(cidade);
+
+
+
+    }
+    public void setBairro(){
+        Console.WriteLine("Digite seu Bairro");
+        string bairro= Console.ReadLine();
+    
+        this.endereco.setBairro(bairro);
+
+
+
+
+    }
+}
